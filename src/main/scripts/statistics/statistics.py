56f8fc2df09c30467018706392eb0f8c9fa9870c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-06-12 16:21:43 +0200

    Updated statistics to work right for the sample page streaming stats

--- src/main/scripts/statistics/statistics.py
+++ src/main/scripts/statistics/statistics.py
@@ -14,6 +14,7 @@ import time
 import cgi
 import cgitb
 import urllib2
+from io import StringIO, BytesIO
 
 # 
 
@@ -21,7 +22,7 @@ config_file_name = "../../statistics.py.cfg"
 
 # -----
 
-cgitb.enable() # web page feedback in case of problems
+#cgitb.enable() # web page feedback in case of problems
 parameters = cgi.FieldStorage()
 
 encoding = "utf-8" # What to convert non-ASCII chars to.
@@ -38,12 +39,12 @@ log_file_pattern = config.get("cgi", "log_file_pattern")
 if "fromDate" in parameters:
     start_str = parameters["fromDate"].value # "2013-06-15"
 else:
-    start_str = "2014-09-01"
+    start_str = "2013-06-01"
 
 if "toDate" in parameters:
     end_str = parameters["toDate"].value
 else:
-    end_str = "2014-12-01"
+    end_str = "2013-07-01"
 
 # http://stackoverflow.com/a/2997846/53897 - 10:00 is to avoid timezone issues in general.
 start_date = datetime.datetime.fromtimestamp(time.mktime(time.strptime(start_str + " 10:00", '%Y-%m-%d %H:%M')))
@@ -65,19 +66,23 @@ handler = urllib2.HTTPBasicAuthHandler(password_mgr)
 opener = urllib2.build_opener(handler)
 
 
+namespaces = {
+    "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "dc":"http://purl.org/dc/elements/1.1/"
+}
+
 # Prepare output CSV:
-fieldnames = ["Timestamp", "Type", "Titel (radio/tv)", "Kanal", "Udsendelsestidspunkt",
-              "Genre", "Titel (reklamefilm)", "Alternativ titel", "Dato", "Reklamefilmstype",
-              "Udgiver", "Klient", "schacHomeOrganization", "eduPersonPrimaryAffiliation",
+fieldnames = ["Timestamp", "Type", "AvisID", "Adgangstype", "Udgivelsestidspunkt", "Udgivelsesnummer",
+              "Sidenummer", "Sektion", "Klient", "schacHomeOrganization", "eduPersonPrimaryAffiliation",
               "eduPersonScopedAffiliation", "eduPersonPrincipalName", "eduPersonTargetedID",
-              "SBIPRoleMapper", "MediestreamFullAccess", "UUID", "URL"]
+              "SBIPRoleMapper", "MediestreamFullAccess", "UUID"]
 
 print "Content-type: text/csv"
 print "Content-disposition: attachment; filename=stat-" + start_str + "-" + end_str + ".csv"
 print
 
 
-result_file = sys.stdout; # open("out.csv", "wb")
+result_file = sys.stdout
 
 result_dict_writer = csv.DictWriter(result_file, fieldnames, delimiter="\t")
 # Inlined result_dict_writer.writeheader() - not present in 2.4.
@@ -86,9 +91,10 @@ header = dict(zip(result_dict_writer.fieldnames, result_dict_writer.fieldnames))
 result_dict_writer.writerow(header)
 
 doms_ids_seen = {} # DOMS lookup cache, id is key
-urls_seen = {} # PLAY event seen yet for this URL? (value is not important)
+uniqueIDs = {} # ticket/domsID combos we have already handled.
 
 for date in dates:
+
     log_file_name = log_file_pattern % date.strftime("%Y-%m-%d")
 
     # Silently skip non-existing logfiles.
@@ -97,95 +103,67 @@ for date in dates:
 
     log_file = open(log_file_name, "rb")
 
-    log_file_dict_reader = csv.DictReader(log_file, delimiter=";")
+    for line in log_file:
+        if not 'AUTHLOG' in line: continue
+        outputLine = {}
 
-    for line in log_file_dict_reader:
-        url = line["Streaming URL"]
-        event = line["Event"]
-        attr = line["User attributes"]
-        ts = line["Timestamp"]
+        outputLine["Type"] = "info:fedora/doms:Newspaper_Collection"
 
-        # Ditte + Mogens rule - we only look at the very first event with the type "PLAY" for each URL.
+        (crap1, json) = line.split("AUTHLOG:")
+        logEntry = simplejson.loads(json)
 
-        if (event != "PLAY"):
-            continue
+        ts = logEntry["dateTime"]
+        outputLine["Timestamp"] = ts
 
-        if url in urls_seen:
+        doms_id = logEntry["resource_id"]
+        outputLine["UUID"] = doms_id
+
+        uniqueID = doms_id + logEntry["ticket_id"]
+        if uniqueID in uniqueIDs:
             continue
         else:
-            urls_seen[url] = ts # only key matters.
+            uniqueIDs[uniqueID] = ts # only key matters.
 
-        # Ok.  Now slowly build row to write in "out"
+        outputLine["Adgangstype"] = logEntry["resource_type"]
 
-        out = { "Timestamp": ts, "URL": url} # add more below
-
-        regexp_match = re_doms_id_from_url.search(url)
-        if regexp_match == None:
-            print "No UUID in URL: " + url + ", line skipped"
-            continue
-
-        doms_id = regexp_match.group(1)
-
-        # big sister probes this, skip (Mogens: if anybody wants to view it, we'll live with it)
-        if doms_id == "d68a0380-012a-4cd8-8e5b-37adf6c2d47f":
-            continue
-
-        out["UUID"] = doms_id
+        outputLine["Klient"] = logEntry["remote_ip"]
 
         if doms_id in doms_ids_seen:
-            (ext_body_text, core_body_text) = doms_ids_seen[doms_id]
+            shortFormat = doms_ids_seen[doms_id]
         else:
-            url_core = doms_url + "objects/uuid%3A" + doms_id + "/datastreams/PBCORE/content"
-            url_ext = doms_url + "objects/uuid%3A" + doms_id + "/datastreams/RELS-EXT/content"
-
-            ext_body = opener.open(url_ext)
-            ext_body_text = ext_body.read()
-            ext_body.close()
+            url_core = doms_url + "?method=simpleSearch&query=pageUUID:\"doms_aviser_page:uuid:"+doms_id+"\"&numberOfRecords=1&startIndex=0"
 
             core_body = opener.open(url_core)
             core_body_text = core_body.read()
             core_body.close()
+            core = ET.fromstring(core_body_text)
+            soapNS = {"soapenv":"http://schemas.xmlsoap.org/soap/envelope/",
+                          "ns1":"http://statsbiblioteket.dk/summa/search"}
+            core_body_text = core.xpath("/soapenv:Envelope/soapenv:Body/simpleSearchResponse/ns1:simpleSearchReturn/text()",namespaces=soapNS)[0]
+            core = ET.parse(BytesIO(bytes(bytearray(core_body_text, encoding='utf-8'))))
+            shortFormat = (core.xpath("/responsecollection/response/documentresult/record[1]/field[@name='shortformat']/shortrecord"))[0]
+            doms_ids_seen[doms_id] = shortFormat
 
-            doms_ids_seen[doms_id] = (ext_body_text, core_body_text)
-
-        namespaces = { "pb": "http://www.pbcore.org/PBCore/PBCoreNamespace.html",
-                       "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                       "sb": "http://doms.statsbiblioteket.dk/relations/default/0/1/#"}
-
-        ext = ET.fromstring(ext_body_text)
-
-        # The (get_list() or [""])[0] construct returns the empty string if the first list is empty
-
-        out["Type"] = (ext.xpath("./rdf:Description/sb:isPartOfCollection/@rdf:resource", namespaces=namespaces) or [""])[0]
-
-        core = ET.fromstring(core_body_text)
-
-        # Radio/TV collection
-        out["Titel (radio/tv)"] = (core.xpath("./pb:pbcoreTitle[pb:titleType/text() = 'titel']/pb:title/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Kanal"] = (core.xpath("./pb:pbcorePublisher[pb:publisherRole/text() = 'kanalnavn']/pb:publisher/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Udsendelsestidspunkt"] = (core.xpath("./pb:pbcoreInstantiation/pb:pbcoreDateAvailable/pb:dateAvailableStart/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Genre"] = (core.xpath("./pb:pbcoreGenre/pb:genre[starts-with(.,'hovedgenre')]/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-
-        # Reklamefilm
-        out["Titel (reklamefilm)"] = (core.xpath("./pb:pbcoreTitle[not(pb:titleType)]/pb:title/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Alternativ titel"] = (core.xpath("./pb:pbcoreTitle[pb:titleType='alternative']/pb:title/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Dato"] = (core.xpath("./pb:pbcoreInstantiation/pb:dateIssued/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Reklamefilmstype"] = (core.xpath("./pb:pbcoreAssetType/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Udgiver"] = (core.xpath("./pb:pbcoreCreator[pb:creatorRole='Producer']/pb:creator/text()", namespaces=namespaces) or [""])[0].encode(encoding)
-        out["Klient"] =  (core.xpath("./pb:pbcoreCreator[pb:creatorRole='Client']/pb:creator/text()", namespaces=namespaces) or [""])[0].encode(encoding)
+        #TODO fix for pdf downloads also, where not all these fields might exist
+        #print ET.tostring(shortFormat)
+        outputLine["AvisID"] = shortFormat.xpath("rdf:RDF/rdf:Description/newspaperTitle/text()",namespaces=namespaces)[0]
+        outputLine["Udgivelsestidspunkt"] = shortFormat.xpath("rdf:RDF/rdf:Description/dateTime/text()",namespaces=namespaces)[0]
+        outputLine["Udgivelsesnummer"] = shortFormat.xpath("rdf:RDF/rdf:Description/newspaperEdition/text()",namespaces=namespaces)[0]
+        outputLine["Sektion"] = shortFormat.xpath("rdf:RDF/rdf:Description/newspaperSection/text()",namespaces=namespaces)[0]
+        outputLine["Sidenummer"] = shortFormat.xpath("rdf:RDF/rdf:Description/newspaperPage/text()",namespaces=namespaces)[0]
 
         # credentials
-        creds = simplejson.loads(attr)
+        creds = logEntry["userAttributes"]
 
         for cred in ["schacHomeOrganization", "eduPersonPrimaryAffiliation",
                      "eduPersonScopedAffiliation", "eduPersonPrincipalName", "eduPersonTargetedID",
                      "SBIPRoleMapper", "MediestreamFullAccess"]:
             if creds and cred in creds:
                 # creds[cred] is list, encode each entry, and join them as a single comma-separated string.
-                out[cred] = ", ".join(e.encode(encoding) for e in creds[cred])
+                outputLine[cred] = ", ".join(e.encode(encoding) for e in creds[cred])
             else:
-                out[cred] = ""
+                outputLine[cred] = ""
 
-        result_dict_writer.writerow(out)
+        result_dict_writer.writerow(outputLine)
 
     log_file.close()
